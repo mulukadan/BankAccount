73b9a94abb2182fa4aac996824b01fba8a50f624
mulukadan <kadan.md@gmail.com>
2017-04-30 02:37:12 +0300

    done logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,60 @@
 $(document).ready(function() {
-      $("form").submit(function(event) {
-        var a = parseInt($("input#sidea").val());
-        var b = parseInt($("input#sideb").val());
-        var c = parseInt($("input#sidec").val());
-        var results="";
+  var name;
+  var accbal;
+  var pin;
 
+      $("#newuser form").submit(function(event) {
+        name = $("input#name").val();
+        pin  = $("input#pin").val();
+        accbal = parseInt($("input#initialbal").val());
 
+        $(".name").text(name);
+        $(".bal").text(accbal);
 
-      if(c> a+b || b>a+c || a>b+c){
-        results = "Not a Triangle";
-      }else {
-        if(a===b && a===c){
-          results = "Equilateral";
-        }else {
-          if(a===b || a===c || b===c ){
-          results = "Isosceles";
-        }
-        else {
-          if(a!=b && a!=c){
-             results = "Scalene";
-          }
-        }
-      }
-      }
-        setImageSource(results);
-        $(".output").text(results);
+        $("#transaction").show();
+        $("#newuser").hide();
 
         event.preventDefault();
       });
 
-      function setImageSource(results) {
-      var imageSrc="";
-      if (results === "Equilateral") {
-         imageSrc="img/e.png";
-      }else if (results === "Isosceles") {
-         imageSrc="img/i.png";
-      }else if (results === "Scalene") {
-         imageSrc="img/s.png";
-      }else {
-        imageSrc="img/n.gif";
-      }
+      $("#transaction form").submit(function(event) {
+        var transaction = $('input[name=transaction]:checked').val();
+        var amt = parseInt($("input#amount").val());
+        var message ="";
 
-        $('#tri').attr('src', imageSrc);
-      
-      }
+        if (transaction === "1") {
+           accbal = accbal + amt;
+           message = "Your transaction has been Successful!!";
+        } else {
+          var Tempaccbal = accbal - amt;
+          if (Tempaccbal < 0) {
+            message = "SORRY: transaction Failed. You do not have sufficient funds";
+          } else {
+            accbal = accbal - amt;
+            message = "Your transaction has been Successful!!"
+          }
+        }
+
+        $(".name").text(name);
+        $(".bal").text(accbal);
+        $(".msg").text(message);
+
+        $("#message").show();
+        $("#transaction").hide();
+        $("#newuser").hide();
+
+        event.preventDefault();
+      });
+
+      $("#back").click(function(event) {
+        $("input#amount").attr("placeholder", "0.00");
+          $("input#amount").val("");
+        $("#message").hide();
+        $("#transaction").show();
+        $("#newuser").hide();
+
+        event.preventDefault();
+      });
+
+
     });
